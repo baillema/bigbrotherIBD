c167f77e1c49e54806e8d37370e05e8e895b35c4
baillema <mrmarthieu@gmail.com>
2016-03-30 15:44:38 +0200

    Ajout du projet de base

---
+++ com.vogella.gae.java.todo/src/com/vogella/gae/java/todo/EmailServlet.java
@@ -0,0 +1,10 @@
+package com.vogella.gae.java.todo;
+import java.util.logging.Logger;
+
+//assumes the current class is called logger
+
+public class EmailServlet {
+	
+	private final static Logger LOGGER = Logger.getLogger(EmailServlet.class.getName());
+
+}
